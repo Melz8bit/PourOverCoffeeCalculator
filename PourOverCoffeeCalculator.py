d9be4251421759687efffffb1dd150005eb3a53a
Melz8bit <37233103+Melz8bit@users.noreply.github.com>
2021-03-31 10:33:56 -0400

    Added functionality to update both coffee and water base amounts

--- PourOverCoffeeCalculator.py
+++ PourOverCoffeeCalculator.py
@@ -3,9 +3,9 @@
 def main():
     # Variables
     # Coffee in grams; water in ml
-    baseCoffee = 55
+    baseCoffee = 24
     userCoffee = 0    
-    baseWater = 1000    
+    baseWater = 709    
     userWater = 0
     userInputOption = ''
     userUpdateBaseOption = ''
@@ -17,13 +17,16 @@ def main():
 
     # Update base amounts, if needed
     if input("Would you like to update the base values? (Y)es/(N)o ").lower() == 'y':
-        while userUpdateBaseOption not in ('c', 'w'):
-            userUpdateBaseOption = input("Would you like to update (C)offee or (W)ater? ").lower()
+        while userUpdateBaseOption not in ('c', 'w', 'b'):
+            userUpdateBaseOption = input("Would you like to update (C)offee, (W)ater, or (B)oth? ").lower()
 
         if userUpdateBaseOption == 'c':
             baseCoffee = int(input('Please enter base coffee amount in grams: '))
         elif userUpdateBaseOption == 'w':
             baseWater = int(input('Please enter base water amount in milliliters: '))
+        elif userUpdateBaseOption == 'b':
+            baseCoffee = int(input('Please enter base coffee amount in grams: '))
+            baseWater = int(input('Please enter base water amount in milliliters: '))            
         
         print('Base coffee: %d grams' % baseCoffee)
         print('Base water: %d ml' % baseWater)
