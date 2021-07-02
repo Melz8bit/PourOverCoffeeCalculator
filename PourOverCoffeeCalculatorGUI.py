115020d6ad6280ff96ea4a2ba5465b83806364bb
Melz <melzdemello@gmail.com>
2021-07-02 19:29:09 -0400

    Created GUI version

---
+++ PourOverCoffeeCalculatorGUI.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+
+# Variables
+# Coffee in grams; water in ml
+baseCoffee = 40
+userCoffee = 0    
+baseWater = 600    
+userWater = 0
+userInputOption = ''
+userUpdateBaseOption = ''
+
+
+def buttonClick(event):
+    userWater = int(textEntry.get())
+    userCoffee = (baseCoffee * userWater) / baseWater
+    print(userCoffee)
+
+window = tk.Tk() # Creates window
+window.title("Pour Over Coffee Calculator") # Sets window title
+window.resizable(0,0) # Disables maximize computer
+window.geometry("600x200") # Sets window size (w x h)
+
+defaultValuesLabel = tk.Label(
+    text="Coffee: %d grams\n Water: %d ml" % (baseCoffee, baseWater)
+)
+
+# Label on screen
+enterCoffeeLabel = tk.Label(
+    text="Please enter the amount of coffee to be used (in grams):",
+    padx=10,
+    pady=10
+)
+
+# Create button
+calculateCoffeeButton = tk.Button(
+    text="Calculate Coffee",
+    height=2,
+    width=12    
+)
+
+# Create textbox
+textEntry = tk.Entry(width=10)
+
+# Puts widgets on screen
+defaultValuesLabel.pack()
+enterCoffeeLabel.pack(side=tk.LEFT)
+textEntry.pack(side=tk.LEFT)
+calculateCoffeeButton.pack(side=tk.BOTTOM)
+
+calculateCoffeeButton.bind("<Button-1>", buttonClick)
+
+# Displays window
+tk.mainloop()
